57fc2d22f441cad8eabcfebcdc3f409e8185203f
jingliyujie <jie.yu@gemii.cc>
2021-09-07 13:39:49 +0800

    第一次提交

---
+++ customer/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,13 @@
+package com.example.demo.entity;
+
+import lombok.Data;
+
+/**
+ * @author yujie
+ * @Date 2021/9/7
+ */
+@Data
+public class User {
+    private String name ;
+    private String address;
+}
